5b9fbf302eba147bc6109336dad615ea7e26778d
Sahiba Kumari <sahibakumari0915@outlook.com>
2022-03-15 21:27:20 +0530

    feat: added questions based on pure functions

--- index.js
+++ index.js
@@ -16,3 +16,29 @@ const newColorsArr = [...colorsArr, "voilet", "pink"];
 console.log(colorsArr);
 console.log(newColorsArr);
 
+// Pure functions
+//Q3.Write a function birthday() to take a person's name and age in an object & increase age.
+
+const birthday = (person) => ({...person, age: person.age+1});
+const sahibaAfterbirthday = birthday(sahiba);
+
+console.log(sahibaAfterbirthday);
+
+//Q4.Write a function which can tell whether a number is less than 10 or not. Suplly this function to array.filter() to get an array with nos.more than 10 in it.
+
+const numbers = [10, 2, 40, 20, 50, 6];
+const isMoreThan10 = num => num > 10;
+const res = numbers.filter(isMoreThan10);
+
+console.log(res);
+
+//Q5.Given an array of numbers, return an array with all numbers incremented by 2.
+
+const incrementedByTwo = numbers.map(num => num + 2);
+console.log(incrementedByTwo);
+
+//
+
+
+
+
